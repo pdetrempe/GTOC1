4fa789680dd3afca6b92cd3c8f5dd1f505292175
Jeff-Robinson <jeffrobinson132@gmail.com>
2023-06-07 00:11:32 -0700

    add M2EH()

--- src/dynamics/keplerian_elements.jl
+++ src/dynamics/keplerian_elements.jl
@@ -13,6 +13,35 @@ function body_osc_elt(;planet::String, epoch::Epoch, frame=default_ref_frame, CB
     return oscltx(body_state, ET, bodvrd(CB,"GM")[1])
 end
 
+function M2EH(;M,ecc,tol=eps(Float64))
+    if ecc < 1
+        EH = M + sign(π-mod(M,2π))*ecc
+        EH₊ = EH # allocate outside loop scope
+        while true # Vallado 4e Algorithm 2 p65
+            EH₊ = EH + (M - EH + ecc*sin(EH))/(1 - ecc*cos(EH))
+            if tol > abs(EH₊ - EH)
+                return EH₊
+            end
+            EH = EH₊
+        end
+    else
+        EH = if ecc < 1.6
+            M + sign(π-mod(M,2π))*ecc
+        elseif (ecc < 3.6) & (abs(M) > π)
+            M - sign(M)*ecc
+        else
+            M/(ecc-1)
+        end
+        EH₊ = EH # allocate outside loop scope
+        while true # Vallado 4e Algorithm 4 p71
+            EH₊ = EH + (M - ecc*sinh(EH) + EH)/(ecc*cosh(EH) - 1)
+            if tol > abs(EH₊ - EH)
+                return EH₊
+            end
+            EH = EH₊
+        end
+    end
+end
 
 # function get_planet_orbit(;planet::String, ET::Epoch, frame="ECLIPJ2000", CB="Sun")
 #     OE = body_osc_elt(;planet=planet, ET=ET, frame=frame, CB=CB)
